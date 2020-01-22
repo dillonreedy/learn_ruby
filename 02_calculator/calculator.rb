b9a3e0dd27bc8c8ba5334dde9de62851f8a5c870
Dillon Reedy <dillon.reedy123@gmail.com>
2020-01-21 22:55:08 -0500

    02_calculator rspec test functions and tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add(number_one, number_two)
+    return number_one+number_two
+end
+
+def subtract(number_one, number_two)
+    return number_one-number_two
+end
+
+def sum(array)
+    sum = 0
+    array.each {|element| sum+=element}
+    return sum
+end
+
+def multiply(number_one, number_two)
+    return number_one*number_two
+end
+
+def product(numbers)
+    result = 1
+    numbers.each {|element| result *= element}
+    return result
+end
+
+def power(base, exponent)
+    return base ** exponent
+end
+
+def factorial(n)
+    if (n == 0 or n == 1) 
+        return 1
+    else
+        return factorial(n-1)*n
+    end
+end

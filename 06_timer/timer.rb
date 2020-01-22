81549d8c42524e02a3941477f1c23e5d4de62f79
Dillon Reedy <dillon.reedy123@gmail.com>
2020-01-22 11:37:15 -0500

    Added solution for rspec tests in 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds/3600
+    remainder = @seconds.modulo(3600)
+    return sprintf("%02d:%02d:%02d", hours, remainder/60, remainder.modulo(60))
+  end
+
 end

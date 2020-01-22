82c20c9d883e122fda4e7df9dcca68e4ebfe424e
Dillon Reedy <dillon.reedy123@gmail.com>
2020-01-21 22:31:01 -0500

    functions written for 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(temperature_farenheit)
+    return ((temperature_farenheit-32) * (5.0 / 9.0))
+end
+
+def ctof(temperature_celsius)
+    return (temperature_celsius * (9.0 / 5.0)) + 32
+end
